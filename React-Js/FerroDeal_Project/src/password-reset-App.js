b91677df0ab9c1f6545bed5fb35fc1bc3f873213
AAYUSH JAIN <aayush.magicalsolutions@gmail.com>
2022-01-12 17:28:37 +0530

    update

--- React-Js/FerroDeal_Project/src/password-reset-App.js
+++ React-Js/FerroDeal_Project/src/password-reset-App.js
@@ -7,20 +7,20 @@ export default class passwordReset extends Component {
   render() {
   return (
     <>
-    <div className="password-reset-outer-space" >
-        <div className='password-reset-auth-inner' >  
+    <div className="password-reset-outer-space-1" >
+        <div className='password-reset-auth-inner-1' >  
 
-        <div className='password-reset-product'>
-        <div className="password-reset-heading">Password Reset</div>
-        <div className="password-reset-para"><p>Enter your email address and we'll send you instructions on how to reset your password.</p></div>    
+        <div className='password-reset-product-1'>
+        <div className="password-reset-heading-1">Password Reset</div>
+        <div className="password-reset-para-1"><p>Enter your email address and we'll send you instructions on how to reset your password.</p></div>    
         
 
-        <div className="password-reset-form-group">
-        <div className="password-reset-email-heading" >Email</div>
-              <input className="password-reset-email-input" type="email" placeholder="Enter your email..."  id="email" name="email"/>
-              <input className="password-reset-submit-button" type="submit"></input>
+        <div className="password-reset-form-group-1">
+        <div className="password-reset-email-heading-1" >Email</div>
+              <input className="password-reset-email-input-1" type="email" placeholder="Enter your email..."  id="email" name="email"/>
+              <input className="password-reset-submit-button-1" type="submit"></input>
             </div>
-            <div className="password-reset-login" >CANCEL</div>
+            <div className="password-reset-login-1" >CANCEL</div>
             </div>  
 </div>    
 </div>  
